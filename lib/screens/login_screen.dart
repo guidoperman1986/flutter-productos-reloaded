232b579bd0824057b57bd6c352d4aafad91c2f4f
Guido <38866056+guidoperman1986@users.noreply.github.com>
2022-05-06 13:52:33 -0300

    loggin page

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:productos/providers/login_form_provider.dart';
+import 'package:productos/ui/input_decorations.dart';
+import 'package:productos/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: AuthBackground(
+            child: SingleChildScrollView(
+      child: Column(children: [
+        const SizedBox(height: 250),
+        CardContainer(
+            child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Text('Login', style: Theme.of(context).textTheme.headline3),
+            const SizedBox(height: 30),
+            ChangeNotifierProvider(
+              create: (_) => LoginFormProvider(),
+              child: LoginForm(),
+            )
+          ],
+        )),
+        const SizedBox(
+          height: 50,
+        ),
+        const Text(
+          'Crear nueva cuenta',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 50)
+      ]),
+    )));
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
+    return Container(
+      child: Form(
+          key: loginForm.formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'john.doe@gmail.com',
+                  labelText: 'Correo Electronico',
+                  prefixIcon: Icons.alternate_email_sharp),
+              onChanged: (value) => loginForm.email = value,
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = new RegExp(pattern);
+
+                return regExp.hasMatch(value ?? '')
+                    ? null
+                    : 'Debe ingresar un correo electronico';
+              },
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              obscureText: true,
+              autocorrect: false,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '**********',
+                  labelText: 'Contraseña',
+                  prefixIcon: Icons.lock_outline),
+              onChanged: (value) => loginForm.password = value,
+              validator: (value) {
+                (value != null && value.length >= 6)
+                    ? null
+                    : 'La contraseña debe tener 6 caracteres minimo';
+              },
+            ),
+            const SizedBox(height: 30),
+            MaterialButton(
+              onPressed: loginForm.isLoading
+                  ? null
+                  : () async {
+                      FocusScope.of(context).unfocus();
+                      if (!loginForm.isValidForm()) return;
+
+                      Future.delayed(Duration(seconds: 2));
+
+                      loginForm.isLoading = false;
+
+                      Navigator.pushReplacementNamed(context, 'home');
+                    },
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text(
+                    loginForm.isLoading ? 'Espere' : 'Ingresar',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            )
+          ])),
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'package:productos/widgets/card_container.dart';
+export 'package:productos/widgets/auth_background.dart';
